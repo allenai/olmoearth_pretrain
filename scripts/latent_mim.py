bb876211b9fe05432ae8f4faccfe1fcc0eb8bc27
Joseph Redmon <pjreddie@gmail.com>
2025-02-28 10:54:53 -0800

    changed workspace

--- scripts/latent_mim.py
+++ scripts/latent_mim.py
@@ -241,6 +241,7 @@ def build_common_components(
         name=f"{run_name}-{cmd_to_launch}",
         cmd=[script, cmd_to_launch, run_name, cluster, *overrides],
         clusters=cluster,
+        workspace="ai2/joer",
         nccl_debug=False,
     )
     root_dir = get_root_dir(cluster)
