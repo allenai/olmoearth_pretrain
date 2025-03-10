bf40014344b416091b2482077fb22cff3d91f31a
Yawen Zhang <yawenz@allenai.org>
2025-03-10 16:08:49 -0700

    change to 4 gpus

--- scripts/latent_mim.py
+++ scripts/latent_mim.py
@@ -90,7 +90,7 @@ def build_train_module_config(
 ) -> LatentMIMTrainModuleConfig:
     """Build the train module config for an experiment."""
     LR = 0.002
-    RANK_MICROBATCH_SIZE = 64
+    RANK_MICROBATCH_SIZE = 128
     ENCODE_RATIO = 0.1
     DECODE_RATIO = 0.75
     WD = 0.02
