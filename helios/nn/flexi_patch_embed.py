1e9f962c5352293e57b12e36df0d123d75f0b09c
hgherzog <henryh@allenai.org>
2025-03-13 16:46:27 -0700

    revert

--- helios/nn/flexi_patch_embed.py
+++ helios/nn/flexi_patch_embed.py
@@ -4,6 +4,7 @@ Extended from: https://github.com/huggingface/pytorch-image-models/blob/main/tim
 by https://github.com/bwconrad/flexivit/
 """
 
+import logging
 from collections.abc import Iterable, Sequence
 from typing import Any
 
@@ -13,6 +14,8 @@ import torch.nn as nn
 import torch.nn.functional as F
 from einops import rearrange
 from torch import Tensor, vmap
+
+logger = logging.getLogger(__name__)
 
 
 class FlexiPatchEmbed(nn.Module):
@@ -197,7 +200,6 @@ class FlexiPatchEmbed(nn.Module):
         else:
             weight = self.resize_patch_embed(self.proj.weight, patch_size)
         # Apply conv with resized weights
-
         x = F.conv2d(x, weight, bias=self.proj.bias, stride=patch_size)
         # At this point x has embedding dim sized channel dimension
         if has_time_dimension:
