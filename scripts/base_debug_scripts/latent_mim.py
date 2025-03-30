f074562b5c49d54b27a1d9b05008abe7966fcfec
Yawen Zhang <yawenz@allenai.org>
2025-03-29 21:08:59 -0700

    fix mock batch

--- scripts/base_debug_scripts/latent_mim.py
+++ scripts/base_debug_scripts/latent_mim.py
@@ -154,9 +154,8 @@ def build_dataloader_config(common: CommonComponents) -> HeliosDataLoaderConfig:
 def build_dataset_config(common: CommonComponents) -> HeliosDatasetConfig:
     """Build the dataset config for an experiment."""
     # NOTE: Change this directory based on the supported modalities
-    h5py_dir = "/weka/dfive-default/helios/dataset/presto/h5py_data/latlon_sentinel1_sentinel2_l2a_worldcover/98856"
     return HeliosDatasetConfig(
-        h5py_dir=h5py_dir,
+        h5py_dir=None,
         tile_path=None,
         supported_modality_names=common.supported_modality_names,
         dtype=DType.float32,
