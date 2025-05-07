aaf828a650b218d8d5962f1ccec5f2fe11d997e9
Yawen Zhang <yawenz@allenai.org>
2025-05-07 11:47:27 -0700

    add new h5 paths

--- scripts/base_debug_scripts/galileo.py
+++ scripts/base_debug_scripts/galileo.py
@@ -184,7 +184,7 @@ def build_dataloader_config(common: CommonComponents) -> HeliosDataLoaderConfig:
 
 def build_dataset_config(common: CommonComponents) -> HeliosDatasetConfig:
     """Build the dataset config for an experiment."""
-    h5py_dir = "/weka/dfive-default/helios/dataset/presto/h5py_data/landsat_naip_openstreetmap_raster_sentinel1_sentinel2_l2a_srtm_worldcover/118861"
+    h5py_dir = "/weka/dfive-default/helios/dataset/presto/h5py_data_gzip_1/landsat_naip_openstreetmap_raster_sentinel1_sentinel2_l2a_srtm_worldcover/118861"
     return HeliosDatasetConfig(
         h5py_dir=h5py_dir,
         training_modalities=common.training_modalities,
