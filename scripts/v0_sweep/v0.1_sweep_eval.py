23f3e214d60f3862b479bc5f58579062f1cc7764
Yawen Zhang <yawenz@allenai.org>
2025-06-05 00:12:05 -0700

    go back to the old linear probing

---
+++ scripts/v0_sweep/v0.1_sweep_eval.py
@@ -0,0 +1,41 @@
+"""Run evaluation sweep for v0.1 models."""
+
+import subprocess  # nosec
+
+LP_LRs = [1e-4]  # , 5e-4, 1e-3, 5e-3, 1e-2, 5e-2, 1e-1, 5e-1]
+
+# v0.1_base_latent_mim_contrastive_random
+base_cmd = (
+    "python scripts/v0_sweep/contrastive_latent_mim.py launch v0.1_base_latent_mim_contrastive_random_{lr} ai2/titan-cirrascale "
+    "--model.decoder_config.depth=4 --common.launch.num_gpus=1 "
+    "--train_module.masking_config.strategy_config.type=random "
+    "--model.reconstructor_config=null --train_module.mae_loss_config=null "
+    "--train_module.ema_decay=[1,1] "
+    "--trainer.load_path=/weka/dfive-default/helios/checkpoints/joer/v0.1_base_latent_mim_contrastive_random__/step157000 "
+)
+
+lr_args = [
+    "--trainer.callbacks.downstream_evaluator.tasks.m-sa-crop-type.probe_lr={lr} "
+    "--trainer.callbacks.downstream_evaluator.tasks.m-cashew-plant.probe_lr={lr} "
+    "--trainer.callbacks.downstream_evaluator.tasks.mados.probe_lr={lr} "
+    "--trainer.callbacks.downstream_evaluator.tasks.sen1floods11.probe_lr={lr} "
+    "--trainer.callbacks.downstream_evaluator.tasks.pastis_sentinel2.probe_lr={lr} "
+    "--trainer.callbacks.downstream_evaluator.tasks.pastis_sentinel1.probe_lr={lr} "
+    "--trainer.callbacks.downstream_evaluator.tasks.pastis_sentinel1_sentinel2.probe_lr={lr} "
+    "--trainer.callbacks.downstream_evaluator.tasks.sickle_sentinel1.probe_lr={lr} "
+    "--trainer.callbacks.downstream_evaluator.tasks.sickle_landsat.probe_lr={lr} "
+    "--trainer.callbacks.downstream_evaluator.tasks.sickle_sentinel1_landsat.probe_lr={lr}"
+]
+
+for lr in LP_LRs:
+    full_cmd = base_cmd.format(lr=lr) + lr_args[0].format(lr=lr)
+    print(f"Running: {full_cmd}")
+    subprocess.run(full_cmd, shell=True)  # nosec
+
+
+# # v0.1_base_latent_mim_space_time
+# python scripts/v0_sweep/latent_mim.py launch v0.1_base_latent_mim_space_time ai2/titan-cirrascale --model.decoder_config.depth=4 --common.launch.num_gpus=1 --train_module.masking_config.strategy_config.type=space_time --model.reconstructor_config=null --train_module.mae_loss_config=null --train_module.ema_decay=\[1,1\] --trainer.load_path=/weka/dfive-default/helios/checkpoints/joer/v0.1_base_latent_mim_space_time/step165000
+
+
+# # v0.1_base_galileo_random_x_space_time
+# python scripts/v0_sweep/galileo.py launch v0.1_base_galileo_random_x_space_time ai2/titan-cirrascale --model.decoder_config.depth=4 --common.launch.num_gpus=1 --train_module.masking_config_a.strategy_config.type=space_time --model.reconstructor_config=null --train_module.mae_loss_config=null --train_module.contrastive_config=null --train_module.ema_decay=\[1,1\] --trainer.load_path=/weka/dfive-default/helios/checkpoints/joer/v0.1_base_galileo_random_x_space_time/step146750
