ab0766b0842fe508d9d2c2abe25b49b68935654f
Joseph Redmon <pjreddie@gmail.com>
2025-06-05 14:42:44 -0700

    guess you do need that

--- scripts/2025_06_03_parameters/latent_mim.py
+++ scripts/2025_06_03_parameters/latent_mim.py
@@ -120,7 +120,7 @@ def build_dataloader_config(common: CommonComponents) -> HeliosDataLoaderConfig:
         global_batch_size=512,
         token_budget=1500,
         prefetch_factor=4,
-        sampled_hw_p_list=range(5, 13),
+        sampled_hw_p_list=list(range(5, 13)),
         min_patch_size=MIN_PATCH_SIZE,
         max_patch_size=MAX_PATCH_SIZE,
         work_dir=common.save_folder,
