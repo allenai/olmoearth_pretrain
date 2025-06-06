901f3bfbad475d5d03054ebfe34b9b31b1ffe34c
Joseph Redmon <pjreddie@gmail.com>
2025-06-06 16:02:45 -0700

    launch

--- scripts/v0.2_sweep/modalities.sh
+++ scripts/v0.2_sweep/modalities.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-python scripts/v0.2_sweep/latent_mim_presto_osm.py dry_run v0.2_latent_mim_space_time_sen2 ai2/jupiter-cirrascale-2 --launch.priority=urgent --common.training_modalities=\[sentinel2_l2a,\] --common.launch.num_gpus=8
-python scripts/v0.2_sweep/latent_mim_presto_osm.py dry_run v0.2_latent_mim_space_time_sen2_sen1 ai2/jupiter-cirrascale-2 --launch.priority=urgent --common.training_modalities=\[sentinel2_l2a,sentinel1,\] --common.launch.num_gpus=8
-python scripts/v0.2_sweep/latent_mim_presto_osm.py dry_run v0.2_latent_mim_space_time_sen2_sen1_landsat_srtm_latlon ai2/jupiter-cirrascale-2 --launch.priority=urgent --common.training_modalities=\[sentinel2_l2a,sentinel1,landsat,srtm,latlon\] --common.launch.num_gpus=8
+python scripts/v0.2_sweep/latent_mim_presto_osm.py launch v0.2_latent_mim_space_time_sen2 ai2/jupiter-cirrascale-2 --launch.priority=urgent --common.training_modalities=\[sentinel2_l2a,\] --common.launch.num_gpus=8
+python scripts/v0.2_sweep/latent_mim_presto_osm.py launch v0.2_latent_mim_space_time_sen2_sen1 ai2/jupiter-cirrascale-2 --launch.priority=urgent --common.training_modalities=\[sentinel2_l2a,sentinel1,\] --common.launch.num_gpus=8
+python scripts/v0.2_sweep/latent_mim_presto_osm.py launch v0.2_latent_mim_space_time_sen2_sen1_landsat_srtm_latlon ai2/jupiter-cirrascale-2 --launch.priority=urgent --common.training_modalities=\[sentinel2_l2a,sentinel1,landsat,srtm,latlon\] --common.launch.num_gpus=8
