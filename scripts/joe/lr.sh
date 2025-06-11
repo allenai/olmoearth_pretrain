d1a71327e62973673698af02c601c9b9670afcc8
Joseph Redmon <pjreddie@gmail.com>
2025-06-11 13:42:47 -0700

    large model

--- scripts/joe/lr.sh
+++ scripts/joe/lr.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 # Titan on 128
-python scripts/joe/latent_mim.py launch latent_mim_lr.0001 ai2/titan-cirrascale --launch.priority=low --common.launch.num_gpus=1 --train_module.optim_config.lr=0.0001
-python scripts/joe/latent_mim.py launch latent_mim_lr.0002 ai2/titan-cirrascale --launch.priority=low --common.launch.num_gpus=1 --train_module.optim_config.lr=0.0002
-python scripts/joe/latent_mim.py launch latent_mim_lr.0004 ai2/titan-cirrascale --launch.priority=low --common.launch.num_gpus=1 --train_module.optim_config.lr=0.0004
-python scripts/joe/latent_mim.py launch latent_mim_lr.001 ai2/titan-cirrascale --launch.priority=low --common.launch.num_gpus=1 --train_module.optim_config.lr=0.001
+#python scripts/joe/latent_mim.py launch latent_mim_lr.0001 ai2/titan-cirrascale --launch.priority=low --common.launch.num_gpus=1 --train_module.optim_config.lr=0.0001
+#python scripts/joe/latent_mim.py launch latent_mim_lr.0002 ai2/titan-cirrascale --launch.priority=low --common.launch.num_gpus=1 --train_module.optim_config.lr=0.0002
+#python scripts/joe/latent_mim.py launch latent_mim_lr.0004 ai2/titan-cirrascale --launch.priority=low --common.launch.num_gpus=1 --train_module.optim_config.lr=0.0004
+#python scripts/joe/latent_mim.py launch latent_mim_lr.001 ai2/titan-cirrascale --launch.priority=low --common.launch.num_gpus=1 --train_module.optim_config.lr=0.001
+python scripts/joe/latent_mim_large.py launch latent_mim_large.001 ai2/jupiter-cirrascale-2 --launch.priority=urgent --common.launch.num_gpus=8 --train_module.optim_config.lr=0.001
+python scripts/joe/latent_mim_large.py launch latent_mim_large.0004 ai2/jupiter-cirrascale-2 --launch.priority=urgent --common.launch.num_gpus=8 --train_module.optim_config.lr=0.0004
