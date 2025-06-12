4a7aa20bfb1c3b17d3e16047ccd5ec463a0a73c6
Favyen Bastani <fbastani@perennate.com>
2025-06-11 20:00:29 -0400

    update sweep script to use fuse name

--- scripts/v0.2_sweep/latent_mim_128_alldata_fuse.py
+++ scripts/v0.2_sweep/latent_mim_128_alldata_fuse.py
@@ -38,7 +38,7 @@ def build_model_config(common: CommonComponents) -> LatentMIMConfig:
         max_patch_size=MAX_PATCH_SIZE,
         drop_path=0.1,
         max_sequence_length=12,
-        pool_layers=3,
+        fuse_layers=3,
     )
     decoder_config = PredictorConfig(
         encoder_embedding_size=model_size["encoder_embedding_size"],
