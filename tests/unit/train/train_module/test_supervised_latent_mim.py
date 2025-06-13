536d468f8925d225a131fb603e1a15df2f9fbe44
Gabriel Tseng <gabriel.tseng@mail.mcgill.ca>
2025-06-13 14:18:42 +0100

    fix test

--- tests/unit/train/train_module/test_supervised_latent_mim.py
+++ tests/unit/train/train_module/test_supervised_latent_mim.py
@@ -1,41 +1,54 @@
 """Unit tests for the supervised latent MIM Training Module."""
 
-import logging
-from pathlib import Path
-from unittest.mock import MagicMock, patch
-
-import pytest
-from torch.nn.functional import one_hot
-from einops import repeat
 import torch
-from olmo_core.optim.adamw import AdamWConfig
-from olmo_core.train.config import TrainerConfig
+from einops import repeat
+from torch.nn.functional import one_hot
 
-from helios.data.constants import Modality
-from helios.data.dataset import HeliosSample, collate_helios
-from helios.data.transform import TransformConfig
-from helios.nn.flexihelios import EncoderConfig, PredictorConfig
-from helios.nn.latent_mim import LatentMIM, LatentMIMConfig
-from helios.train.loss import LossConfig
-from helios.train.masking import MaskingConfig
-from helios.train.train_module.supervised_latent_mim import SupervisedLatentMIMTrainModule
+from helios.train.train_module.supervised_latent_mim import (
+    SupervisedLatentMIMTrainModule,
+)
 
 
 class TestSupervisedLatentMIMUnit:
+    """Unit tests for the supervised latent MIM Training Module."""
+
     def test_supervised_loss(self) -> None:
+        """Test the supervised loss."""
         max_patch_size = 12
         batch_patch_size = 3
-        b, h, w, = 1, 2, 2
+        b = 1
         supervisory_modalities = {
             # 95 will be assigned class 11
-            "worldcover": repeat(torch.tensor([[95, 95], [95, 70]]), "h w -> b (h p1) (w p2) t d", b=b, p1=batch_patch_size, p2=batch_patch_size, t=1, d=1)
+            "worldcover": repeat(
+                torch.tensor([[95, 95], [95, 70]]),
+                "h w -> b (h p1) (w p2) t d",
+                b=b,
+                p1=batch_patch_size,
+                p2=batch_patch_size,
+                t=1,
+                d=1,
+            )
         }
         # 1s where the value is present
         probe_outputs = {
-            "mask": repeat(torch.tensor([[1, 0], [0, 1]]), "h w -> b (h p1) (w p2)", b=b, p1=max_patch_size, p2=max_patch_size),
-            "worldcover_0": repeat(one_hot(torch.tensor([[4, 11], [11, 7]]), num_classes=12).float(), "h w d -> b (p1 h) (p2 w) d", b=b, p1=max_patch_size, p2=max_patch_size)
+            "mask": repeat(
+                torch.tensor([[1, 0], [0, 1]]),
+                "h w -> b (h p1) (w p2)",
+                b=b,
+                p1=max_patch_size,
+                p2=max_patch_size,
+            ),
+            "worldcover_0": repeat(
+                # times 100 since this is unnormalized from the perspective of the ce loss
+                one_hot(torch.tensor([[4, 11], [11, 7]]), num_classes=12).float() * 100,
+                "h w d -> b (h p1) (w p2) d",
+                b=b,
+                p1=max_patch_size,
+                p2=max_patch_size,
+            ),
         }
         org_loss = torch.tensor(0).float()
-        loss = SupervisedLatentMIMTrainModule.supervisory_losses(supervisory_modalities, probe_outputs, org_loss)
-        print(loss)
+        loss = SupervisedLatentMIMTrainModule.supervisory_losses(
+            supervisory_modalities, probe_outputs, org_loss
+        )
         assert loss == 0
