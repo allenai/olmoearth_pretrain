8889b78319365bb59809f33214a4069028cd61bc
Gabriel Tseng <gabriel.tseng@mail.mcgill.ca>
2025-06-13 18:48:40 +0100

    Fix tests

--- tests/unit/train/train_module/test_supervised_latent_mim.py
+++ tests/unit/train/train_module/test_supervised_latent_mim.py
@@ -31,12 +31,13 @@ class TestSupervisedLatentMIMUnit:
         }
         # 1s where the value is present
         probe_outputs = {
+            # the mask has the INPUT size, not the OUTPUT size
             "mask": repeat(
                 torch.tensor([[1, 0], [0, 1]]),
                 "h w -> b (h p1) (w p2)",
                 b=b,
-                p1=max_patch_size,
-                p2=max_patch_size,
+                p1=batch_patch_size,
+                p2=batch_patch_size,
             ),
             "worldcover_0": repeat(
                 # times 100 since this is unnormalized from the perspective of the ce loss
