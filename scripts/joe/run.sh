6bf32fd64abf6e1e070571deba565c12b5483c2c
Joseph Redmon <pjreddie@gmail.com>
2025-06-25 14:11:53 -0700

    try without

--- scripts/joe/run.sh
+++ scripts/joe/run.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 #python scripts/joe/latent_mim.py launch latent_mim_batch_contrastive_test ai2/titan-cirrascale --launch.priority=urgent --common.launch.num_gpus=1
-python scripts/joe/latent_mim.py launch latent_mim_batch_contrastive_tau_2 ai2/titan-cirrascale --launch.priority=urgent --common.launch.num_gpus=8 --train_module.regularizer_config.loss_config.tau=1.0
+#python scripts/joe/latent_mim.py launch latent_mim_batch_contrastive_tau_2 ai2/titan-cirrascale --launch.priority=urgent --common.launch.num_gpus=8 --train_module.regularizer_config.loss_config.tau=1.0
+python scripts/joe/latent_mim.py launch latent_mim_no_batch_contrastive ai2/titan-cirrascale --launch.priority=high --common.launch.num_gpus=8 --train_module.regularizer_config.loss_config.weight=0
