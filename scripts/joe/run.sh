0376b6476b0560083060bef376789697eb6377df
Joseph Redmon <pjreddie@gmail.com>
2025-06-25 10:23:00 -0700

    version

--- scripts/joe/run.sh
+++ scripts/joe/run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 #python scripts/joe/latent_mim.py launch latent_mim_batch_contrastive_test ai2/titan-cirrascale --launch.priority=urgent --common.launch.num_gpus=1
-python scripts/joe/latent_mim.py launch latent_mim_batch_contrastive_tau_1 ai2/titan-cirrascale --launch.priority=urgent --common.launch.num_gpus=8 --train_module.regularizer_config.loss_config.tau=1.0
+python scripts/joe/latent_mim.py launch latent_mim_batch_contrastive_tau_2 ai2/titan-cirrascale --launch.priority=urgent --common.launch.num_gpus=8 --train_module.regularizer_config.loss_config.tau=1.0
