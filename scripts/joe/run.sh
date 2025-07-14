5800c19afb94e01b2b145d614e4985e6cc9482c2
Joseph Redmon <pjreddie@gmail.com>
2025-07-14 09:46:34 -0700

    for saturn

--- scripts/joe/run.sh
+++ scripts/joe/run.sh
@@ -3,4 +3,4 @@
 #python scripts/joe/latent_mim.py launch latent_mim_batch_contrastive_test ai2/titan-cirrascale --launch.priority=urgent --common.launch.num_gpus=1
 #python scripts/joe/latent_mim.py launch latent_mim_batch_contrastive_tau_2 ai2/titan-cirrascale --launch.priority=urgent --common.launch.num_gpus=8 --train_module.regularizer_config.loss_config.tau=1.0
 python scripts/joe/latent_mim.py launch latent_mim_no_batch_contrastive ai2/titan-cirrascale --launch.priority=high --common.launch.num_gpus=8 --train_module.regularizer_config.loss_config.weight=0
-python scripts/joe/galileo.py launch galileo_all_disc_cross_space_time ai2/titan-cirrascale --launch.priority=high --common.launch.num_gpus=8
+python scripts/joe/galileo.py launch galileo_all_disc_cross_space_time_new_saturn ai2/saturn-cirrascale --launch.priority=high --common.launch.num_gpus=8
