d4706352dd984d37e339646fbd2e4b92eb4e2e74
root <root@neptune-cs-aus-260.reviz.ai2.in>
2025-07-30 17:00:23 -0700

    decode all

--- scripts/2025_07_21_add_era5/latent_mim_add_era5.py
+++ scripts/2025_07_21_add_era5/latent_mim_add_era5.py
@@ -139,12 +139,12 @@ def build_train_module_config(
                 "decode_ratio": 0.5,
                 "allow_encoding_decoding_same_bandset": True,
                 "min_decoded_bandsets": 6,
-                "only_decode_modalities": [
-                    Modality.OPENSTREETMAP_RASTER.name,
-                    Modality.WORLDCOVER.name,
-                    Modality.SRTM.name,
-                    Modality.ERA5_10.name,
-                ],
+                # "only_decode_modalities": [
+                #     Modality.OPENSTREETMAP_RASTER.name,
+                #     Modality.WORLDCOVER.name,
+                #     Modality.SRTM.name,
+                #     Modality.ERA5_10.name,
+                # ],
             }
         ),
         loss_config=LossConfig(
