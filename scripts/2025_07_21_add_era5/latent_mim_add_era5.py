79e78018beeff34cfd89467946413425b4bb9e2c
Yawen Zhang <yawenz@allenai.org>
2025-08-01 23:00:01 -0700

    add cross

--- scripts/2025_07_21_add_era5/latent_mim_add_era5.py
+++ scripts/2025_07_21_add_era5/latent_mim_add_era5.py
@@ -169,7 +169,7 @@ def build_dataloader_config(common: CommonComponents) -> HeliosDataLoaderConfig:
         global_batch_size=512,
         token_budget=1500,
         prefetch_factor=4,
-        sampled_hw_p_list=list(range(5, 12)),  # try only temporal tokens
+        sampled_hw_p_list=list(range(5, 6)),  # try only temporal tokens
         min_patch_size=MIN_PATCH_SIZE,
         max_patch_size=MAX_PATCH_SIZE,
         work_dir=common.save_folder,
