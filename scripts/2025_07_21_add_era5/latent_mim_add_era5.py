705f6004c24fbc20528a2353d02db695d6ec2c84
Yawen Zhang <yawenz@allenai.org>
2025-08-07 15:24:04 -0700

    try again

--- scripts/2025_07_21_add_era5/latent_mim_add_era5.py
+++ scripts/2025_07_21_add_era5/latent_mim_add_era5.py
@@ -155,7 +155,7 @@ def build_train_module_config(
         # ),
         loss_config=LossConfig(
             loss_config={
-                "type": "modality_all_discrimination",
+                "type": "modality_patch_discrimination_new",
             }
         ),
         token_exit_cfg={modality: 0 for modality in common.training_modalities},
@@ -281,7 +281,7 @@ def build_trainer_config(common: CommonComponents) -> TrainerConfig:
             metrics_collect_interval=METRICS_COLLECT_INTERVAL,
             max_duration=MAX_DURATION,
             checkpointer=checkpointer_config,
-            # load_path="/weka/dfive-default/helios/checkpoints/yawenzzzz/latent_mim_cross_random_per_modality_patchdisc_loss/step185000",
+            load_path="/weka/dfive-default/helios/checkpoints/yawenzzzz/latent_mim_cross_random_per_modality_patchdisc_loss/step185000",
         )
         .with_callback("wandb", wandb_callback)
         .with_callback("speed_monitor", HeliosSpeedMonitorCallback())
