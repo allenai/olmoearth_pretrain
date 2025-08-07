65344bd67b21a148c4fbd1f233de828c9e40d465
hgherzog <henryh@allenai.org>
2025-08-07 00:46:53 +0000

    modality cross random from supervisory

--- helios/train/loss.py
+++ helios/train/loss.py
@@ -169,7 +169,7 @@ class PatchDiscriminationLossNew(Loss):
             end = start + c
             if c == 0:
                 # we will occasionally get a sample with no decoded values due to missing data this will let us skip it
-                logger.warning("No decoded values for this sample")
+                logger.debug("No decoded values for this sample")
                 continue
             pred_sample = pred[:, start:end, :]
             target_sample = target[:, start:end, :]
