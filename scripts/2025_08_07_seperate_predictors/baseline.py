b660f405e5e979638313f3ac0fe61959c00b72dc
hgherzog <henryh@allenai.org>
2025-08-07 17:51:30 -0700

    2025 -8 07 map cotrain

--- scripts/2025_08_07_seperate_predictors/baseline.py
+++ scripts/2025_08_07_seperate_predictors/baseline.py
@@ -174,7 +174,7 @@ def build_trainer_config(common: CommonComponents) -> TrainerConfig:
     CANCEL_CHECK_INTERVAL = 25
     LOAD_STRATEGY = LoadStrategy.if_available
     WANDB_USERNAME = "eai-ai2"  # nosec
-    WANDB_PROJECT = "2025_07_10_no_map_ssl"
+    WANDB_PROJECT = "2025_08_07_map_cotrain"
     PERMANENT_SAVE_INTERVAL = 5000
     EPHERMERAL_SAVE_INTERVAL = 250
     checkpointer_config = CheckpointerConfig(work_dir=common.save_folder)
