0b39ef6640e384ba79d6046db2b0ab8ac6374b97
root <root@neptune-cs-aus-261.reviz.ai2.in>
2025-08-11 22:17:07 -0700

    change to new loss

--- scripts/2025_07_21_add_era5/latent_mim_add_era5.py
+++ scripts/2025_07_21_add_era5/latent_mim_add_era5.py
@@ -124,7 +124,7 @@ def build_train_module_config(
         ),
         loss_config=LossConfig(
             loss_config={
-                "type": "modality_patch_discrimination_new",
+                "type": "modality_patch_discrimination_new_hard",
                 "tau": 0.1,
             }
         ),
