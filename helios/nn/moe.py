35f9109c15184810725c7075c5901a35a6a92412
Gabriel Tseng <gabriel.tseng@mail.mcgill.ca>
2025-08-13 11:32:07 +0200

    compute loss against instances, not tokens

--- helios/nn/moe.py
+++ helios/nn/moe.py
@@ -6,7 +6,7 @@ from dataclasses import dataclass
 
 import torch
 import torch.nn as nn
-from einops import rearrange, repeat
+from einops import rearrange
 from olmo_core.config import Config
 from torch import Tensor
 
@@ -40,7 +40,6 @@ class SwitchFeedForward(nn.Module):
         *,
         capacity_factor: float = 1.0,
         drop_tokens: bool = False,
-        is_scale_prob: bool = True,
         n_experts: int = 4,
         expert: Mlp,
         d_model: int,
@@ -50,7 +49,6 @@ class SwitchFeedForward(nn.Module):
 
         * `capacity_factor` is the capacity of each expert as a factor relative to ideally balanced load
         * `drop_tokens` specifies whether to drop tokens if more tokens are routed to an expert than the capacity
-        * `is_scale_prob` specifies whether to multiply the input to the FFN by the routing probability
         * `n_experts` is the number of experts
         * `expert` is the expert layer, a [FFN module](../feed_forward.html)
         * `d_model` is the number of features in a token embedding
@@ -60,7 +58,6 @@ class SwitchFeedForward(nn.Module):
         super().__init__()
 
         self.capacity_factor = capacity_factor
-        self.is_scale_prob = is_scale_prob
         self.n_experts = n_experts
         self.drop_tokens = drop_tokens
 
@@ -75,89 +72,113 @@ class SwitchFeedForward(nn.Module):
         self, x: torch.Tensor, routing_tokens: Tensor
     ) -> tuple[Tensor, Tensor, Tensor, int, Tensor]:
         """X is the input to the switching module with shape `[batch_size, seq_len, d_model]`."""
-        x = rearrange(x, "b n d -> n b d")
-        n = x.shape[0]
-        # Capture the shape to change shapes later
-        seq_len, batch_size, d_model = x.shape
-        # Flatten the sequence and batch dimensions
-        # this flattening happens as [B1, B1, B1, ... B_N, B_N, B_N]
-        x = x.reshape(-1, d_model)
         # Get routing probabilities for each of the tokens.
         # $$p_i(x) = \frac{e^{h(x)_i}}{\sum^N_j e^{h(x)_j}}$$
         # where $N$ is the number of experts `n_experts` and
         # $h(\cdot)$ is the linear transformation of token embeddings.
+
+        # Initialize an empty list of dropped tokens
+        dropped: list[Tensor] = []
+
         if self.route_with_latlons:
             # assuming only a single routing token
             routing_tokens = routing_tokens[:, 0]  # B, D
-            route_prob = self.softmax(self.switch(routing_tokens))
-            route_prob = repeat(route_prob, "b d -> (repeat b) d", repeat=n)
+            route_prob = self.softmax(self.switch(routing_tokens))  # B, D
+            # we will index according to batches, so no need to flatten
+            # Get indexes of tokens going to each expert
+
+            # Get the maximum routing probabilities and the routes.
+            # We route to the expert with highest probability
+            route_prob_max, routes = torch.max(route_prob, dim=-1)
+
+            indexes_list = [
+                torch.eq(routes, i).nonzero(as_tuple=True)[0]
+                for i in range(self.n_experts)
+            ]  # B, num_experts
+
+            # Initialize an empty tensor to store outputs
+            final_output = x.new_zeros(x.shape)
+            if self.drop_tokens:
+                raise ValueError("drop_tokens unsupported for route_with_latlons")
+            # Number of *instances* routed to each expert.
+            counts = x.new_tensor([len(indexes_list[i]) for i in range(self.n_experts)])
+            # Get outputs of the expert FFNs
+            expert_output = [
+                self.experts[i](x[indexes_list[i], :, :]) for i in range(self.n_experts)
+            ]
+            # Assign to final output
+            for i in range(self.n_experts):
+                final_output[indexes_list[i], :] = expert_output[i]
+            # Multiply by the expert outputs by the probabilities $y = p_i(x) E_i(x)$
+            final_output = final_output * route_prob_max.view(-1, 1, 1)
+
         else:
+            x = rearrange(x, "b n d -> n b d")
+            # Capture the shape to change shapes later
+            seq_len, batch_size, d_model = x.shape
+            # Flatten the sequence and batch dimensions
+            # this flattening happens as [B1, B1, B1, ... B_N, B_N, B_N]
+            x = x.reshape(-1, d_model)
             route_prob = self.softmax(self.switch(x))
 
-        # Get the maximum routing probabilities and the routes.
-        # We route to the expert with highest probability
-        route_prob_max, routes = torch.max(route_prob, dim=-1)
+            # Get the maximum routing probabilities and the routes.
+            # We route to the expert with highest probability
+            route_prob_max, routes = torch.max(route_prob, dim=-1)
 
-        # Get indexes of tokens going to each expert
-        indexes_list = [
-            torch.eq(routes, i).nonzero(as_tuple=True)[0] for i in range(self.n_experts)
-        ]
+            # Get indexes of tokens going to each expert
+            indexes_list = [
+                torch.eq(routes, i).nonzero(as_tuple=True)[0]
+                for i in range(self.n_experts)
+            ]
 
-        # Initialize an empty tensor to store outputs
-        final_output = x.new_zeros(x.shape)
+            # Initialize an empty tensor to store outputs
+            final_output = x.new_zeros(x.shape)
 
-        # Capacity of each expert.
-        # $$\mathrm{expert\;capacity} =
-        # \frac{\mathrm{tokens\;per\;batch}}{\mathrm{number\;of\;experts}}
-        # \times \mathrm{capacity\;factor}$$
-        capacity = int(self.capacity_factor * len(x) / self.n_experts)
-        # Number of tokens routed to each expert.
-        counts = x.new_tensor([len(indexes_list[i]) for i in range(self.n_experts)])
+            # Capacity of each expert.
+            # $$\mathrm{expert\;capacity} =
+            # \frac{\mathrm{tokens\;per\;batch}}{\mathrm{number\;of\;experts}}
+            # \times \mathrm{capacity\;factor}$$
+            capacity = int(self.capacity_factor * len(x) / self.n_experts)
+            # Number of tokens routed to each expert.
+            counts = x.new_tensor([len(indexes_list[i]) for i in range(self.n_experts)])
 
-        # Initialize an empty list of dropped tokens
-        dropped = []
-        # Only drop tokens if `drop_tokens` is `True`.
-        if self.drop_tokens:
-            # Drop tokens in each of the experts
+            # Only drop tokens if `drop_tokens` is `True`.
+            if self.drop_tokens:
+                # Drop tokens in each of the experts
+                for i in range(self.n_experts):
+                    # Ignore if the expert is not over capacity
+                    if len(indexes_list[i]) <= capacity:
+                        continue
+                    # Shuffle indexes before dropping
+                    indexes_list[i] = indexes_list[i][
+                        torch.randperm(len(indexes_list[i]))
+                    ]
+                    # Collect the tokens over capacity as dropped tokens
+                    dropped.append(indexes_list[i][capacity:])
+                    # Keep only the tokens upto the capacity of the expert
+                    indexes_list[i] = indexes_list[i][:capacity]
+
+            # Get outputs of the expert FFNs
+            expert_output = [
+                self.experts[i](x[indexes_list[i], :]) for i in range(self.n_experts)
+            ]
+
+            # Assign to final output
             for i in range(self.n_experts):
-                # Ignore if the expert is not over capacity
-                if len(indexes_list[i]) <= capacity:
-                    continue
-                # Shuffle indexes before dropping
-                indexes_list[i] = indexes_list[i][torch.randperm(len(indexes_list[i]))]
-                # Collect the tokens over capacity as dropped tokens
-                dropped.append(indexes_list[i][capacity:])
-                # Keep only the tokens upto the capacity of the expert
-                indexes_list[i] = indexes_list[i][:capacity]
+                final_output[indexes_list[i], :] = expert_output[i]
 
-        # Get outputs of the expert FFNs
-        expert_output = [
-            self.experts[i](x[indexes_list[i], :]) for i in range(self.n_experts)
-        ]
+            # Pass through the dropped tokens
+            if dropped:
+                dropped = torch.cat(dropped)
+                final_output[dropped, :] = x[dropped, :]
 
-        # Assign to final output
-        for i in range(self.n_experts):
-            final_output[indexes_list[i], :] = expert_output[i]
-
-        # Pass through the dropped tokens
-        if dropped:
-            dropped = torch.cat(dropped)
-            final_output[dropped, :] = x[dropped, :]
-
-        if self.is_scale_prob:
             # Multiply by the expert outputs by the probabilities $y = p_i(x) E_i(x)$
             final_output = final_output * route_prob_max.view(-1, 1)
-        else:
-            # Don't scale the values but multiply by $\frac{p}{\hat{p}} = 1$ so that the gradients flow
-            # (this is something we experimented with).
-            final_output = final_output * (
-                route_prob_max / route_prob_max.detach()
-            ).view(-1, 1)
 
-        # Change the shape of the final output back to `[batch_size, seq_len, d_model]`
-        final_output = rearrange(
-            final_output.view(seq_len, batch_size, d_model), "n b d -> b n d"
-        )
+            # Change the shape of the final output back to `[batch_size, seq_len, d_model]`
+            final_output = rearrange(
+                final_output.view(seq_len, batch_size, d_model), "n b d -> b n d"
+            )
 
         # Return
         #
