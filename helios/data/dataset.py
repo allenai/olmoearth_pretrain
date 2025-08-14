e736fb4cb6b39a657cb06221304573d972ca7175
Gabriel Tseng <gabriel.tseng@mail.mcgill.ca>
2025-08-14 16:51:55 +0200

    Add throughout

--- helios/data/dataset.py
+++ helios/data/dataset.py
@@ -57,6 +57,7 @@ class HeliosSample(NamedTuple):
     naip_10: ArrayTensor | None = None  # [B, H, W, T, len(NAIP_bands)]
     gse: ArrayTensor | None = None  # [B, H, W, 1, len(GSE_bands)]
     cdl: ArrayTensor | None = None  # [B, H, W, 1, len(CDL_bands)]
+    worldcereal: ArrayTensor | None = None  # [B, H, W, 1, len(CDL_bands)]
 
     # TODO: Add unit tests for this
     def shape(self, attribute: str, mask: bool = False) -> Sequence[int]:
--- helios/nn/flexihelios.py
+++ helios/nn/flexihelios.py
@@ -95,6 +95,8 @@ class TokensAndMasks(NamedTuple):
     gse_mask: Tensor | None = None
     cdl: Tensor | None = None
     cdl_mask: Tensor | None = None
+    worldcereal: Tensor | None = None
+    worldcereal_mask: Tensor | None = None
 
     @property
     def device(self) -> torch.device:
