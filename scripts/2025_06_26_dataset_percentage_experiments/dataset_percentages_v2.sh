658f8ab9946c99fd21828005658f7b3fad7daff6
hgherzog <henryh@allenai.org>
2025-08-15 16:53:27 -0700

    ready to launch large dataset experiments

--- scripts/2025_06_26_dataset_percentage_experiments/dataset_percentages_v2.sh
+++ scripts/2025_06_26_dataset_percentage_experiments/dataset_percentages_v2.sh
@@ -3,15 +3,18 @@
 # Script to launch latent_mim_all_data.py with varying dataset percentages
 
 PERCENTAGES=(
-    0.00004
+    # 0.00004
     0.0001
-    # 0.0004
-    # 0.004
+    0.0004
+    0.004
     # 0.0078125
     # 0.015625
-    # 0.03125
+    0.03125
     # 0.0625
-    # 0.125
+    0.125
+    # 0.25
+    0.5
+    1.0
 )
 # for i in $(seq $(( (${#PERCENTAGES[@]} + 1) / 2 )) $((${#PERCENTAGES[@]} - 1))); do
 #     pct="${PERCENTAGES[$i]}"
@@ -24,7 +27,8 @@ PERCENTAGES=(
 for pct in "${PERCENTAGES[@]}"; do
     # Remove leading zero for name, keep full precision
     pct_str=$(printf "%.8f" "$pct" | sed 's/^0*//;s/\.$//;s/0*$//')
-    name="latent_mim_cross_only_dec_wc_osm_srtm_dataset_percentage_sweep_${pct_str}"
+    name="latent_mim_cross_largeonly_dec_wc_osm_srtm_dataset_percentage_sweep_${pct_str}"
+    num_epochs=$((75 / $pct))
     echo "Launching for percentage $pct as $name"
-    python3 scripts/2025_06_26_dataset_percentage_experiments/latent_mim_all_data.py launch "$name" ai2/saturn-cirrascale --launch.priority=high --launch.num_gpus=8 --common.dataset_percentage="$pct"
+    python3 scripts/2025_06_26_dataset_percentage_experiments/cross_large.py launch "$name" ai2/jupiter-cirrascale-2 --launch.priority=high --launch.num_gpus=8 --common.dataset_percentage="$pct"
 done
