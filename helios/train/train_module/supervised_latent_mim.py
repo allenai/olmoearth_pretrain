e38aba2bee74daac95c628240c31b5b71bdc0cf3
Gabriel Tseng <gabriel.tseng@mail.mcgill.ca>
2025-08-15 14:36:58 +0200

    bce loss targets should be floats, confusingly

--- helios/train/train_module/supervised_latent_mim.py
+++ helios/train/train_module/supervised_latent_mim.py
@@ -275,9 +275,9 @@ class SupervisedLatentMIMTrainModule(HeliosTrainModule):
                             modality_bandset = torch.argmax(
                                 modality_bandset, dim=-1, keepdim=True
                             )
+                            modality_bandset = modality_bandset.long()
                     else:
                         print("wc unique values, ", modality_bandset.unique())
-                    modality_bandset = modality_bandset.long()
                 else:
                     modality_bandset = modality_bandset.to(dtype=probe_output.dtype)
 
