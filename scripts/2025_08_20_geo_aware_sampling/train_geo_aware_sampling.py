9ed8bcfccf3d6ef0ede64787055fe745136f725c
hgherzog <henryh@allenai.org>
2025-08-20 21:17:14 +0000

    infinite schedule

--- scripts/2025_08_20_geo_aware_sampling/train_geo_aware_sampling.py
+++ scripts/2025_08_20_geo_aware_sampling/train_geo_aware_sampling.py
@@ -201,7 +201,7 @@ def build_dataset_config(common: CommonComponents) -> HeliosDatasetConfig:
 
 def build_trainer_config(common: CommonComponents) -> TrainerConfig:
     """Build the trainer config for an experiment."""
-    MAX_DURATION = Duration.epochs(300)
+    MAX_DURATION = Duration.epochs(500)
     METRICS_COLLECT_INTERVAL = 10
     CANCEL_CHECK_INTERVAL = 25
     LOAD_STRATEGY = LoadStrategy.if_available
