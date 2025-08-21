2abdc9f594e82361f157d2b11bf492fa150a38bc
Gabriel Tseng <gabriel.tseng@mail.mcgill.ca>
2025-08-21 08:23:39 +0200

    no probes

--- scripts/2025_08_14_worldcereal/train_supervised_contrastive.py
+++ scripts/2025_08_14_worldcereal/train_supervised_contrastive.py
@@ -114,8 +114,8 @@ def build_model_config(common: CommonComponents) -> LatentMIMConfig:
         probe_modalities=list(SUPERVISORY_MODALITIES.keys()),
         use_spatial_attn=False,
         probe_dims=[
-            model_size["encoder_embedding_size"],
-            model_size["encoder_embedding_size"],
+            # model_size["encoder_embedding_size"],
+            # model_size["encoder_embedding_size"],
         ],
     )
     decoder_config = PredictorConfig(
