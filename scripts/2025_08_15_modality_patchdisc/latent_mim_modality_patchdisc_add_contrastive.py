4608a83446f5456f246e892c4070e05ae8c4cd67
Yawen Zhang <yawenz@allenai.org>
2025-08-28 14:01:13 -0700

    try again

--- scripts/2025_08_15_modality_patchdisc/latent_mim_modality_patchdisc_add_contrastive.py
+++ scripts/2025_08_15_modality_patchdisc/latent_mim_modality_patchdisc_add_contrastive.py
@@ -143,8 +143,8 @@ def build_train_module_config(
             }
         ),
         token_exit_cfg={modality: 0 for modality in common.training_modalities},
-        max_grad_norm=0.1,
-        scheduler=CosWithWarmup(warmup_steps=8000),
+        max_grad_norm=0.5,
+        scheduler=CosWithWarmup(warmup_steps=20000),
         ema_decay=(1.0, 1.0),
         dp_config=DataParallelConfig(
             name=DataParallelType.fsdp,
