f7bd6bc56d0cdb398235488bba9907716e44a07f
Gabriel Tseng <gabriel.tseng@mail.mcgill.ca>
2025-08-31 19:45:46 +0200

    no shared mlp

--- scripts/2025_08_14_worldcereal/train_supervised_contrastive.py
+++ scripts/2025_08_14_worldcereal/train_supervised_contrastive.py
@@ -114,7 +114,9 @@ def build_model_config(common: CommonComponents) -> LatentMIMConfig:
         drop_path=0.1,
         max_sequence_length=12,
         probe_modalities=list(SUPERVISORY_MODALITIES.keys()),
-        shared_linear_layer_dims=[model_size["encoder_embedding_size"]],
+        shared_linear_layer_dims=[
+            # model_size["encoder_embedding_size"]
+        ],
         use_spatial_attn=True,
         probe_dims=[
             # model_size["encoder_embedding_size"],
