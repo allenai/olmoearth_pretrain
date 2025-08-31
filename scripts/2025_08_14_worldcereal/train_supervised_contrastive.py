636fcd25b7e59056c532c62db7f67cb1d9e0315c
Gabriel Tseng <gabriel.tseng@mail.mcgill.ca>
2025-08-31 08:35:04 +0200

    not trainable probes

--- scripts/2025_08_14_worldcereal/train_supervised_contrastive.py
+++ scripts/2025_08_14_worldcereal/train_supervised_contrastive.py
@@ -114,15 +114,13 @@ def build_model_config(common: CommonComponents) -> LatentMIMConfig:
         drop_path=0.1,
         max_sequence_length=12,
         probe_modalities=list(SUPERVISORY_MODALITIES.keys()),
-        shared_linear_layer_dims=[
-            # model_size["encoder_embedding_size"]
-        ],
+        shared_linear_layer_dims=[model_size["encoder_embedding_size"]],
         use_spatial_attn=True,
         probe_dims=[
             # model_size["encoder_embedding_size"],
             # model_size["encoder_embedding_size"],
         ],
-        num_queries=5,
+        trainable_probes=False,
     )
     decoder_config = PredictorConfig(
         encoder_embedding_size=model_size["encoder_embedding_size"],
