9df5fa11100a1ec4edcb138bafa02572967a3805
Yawen Zhang <yawenz@allenai.org>
2025-09-08 21:28:57 -0700

    switch

--- scripts/2025_09_08_modality_attention/latent_mim_switch_full_modality.py
+++ scripts/2025_09_08_modality_attention/latent_mim_switch_full_modality.py
@@ -94,8 +94,9 @@ def build_model_config(common: CommonComponents) -> LatentMIMConfig:
         drop_path=0.1,
         max_sequence_length=12,
         # layer_attention_modes=["MODALITY"] * model_size["encoder_depth"],
-        layer_attention_modes=["MODALITY"] * 6
-        + ["FULL"] * (model_size["encoder_depth"] - 6),
+        # layer_attention_modes=["MODALITY"] * 6
+        # + ["FULL"] * (model_size["encoder_depth"] - 6),
+        layer_attention_modes=["MODALITY", "MODALITY", "FULL"] * 4,
     )
     decoder_config = STPredictorConfig(
         encoder_embedding_size=model_size["encoder_embedding_size"],
