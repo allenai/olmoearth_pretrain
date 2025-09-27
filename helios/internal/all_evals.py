a3bc0406b6881b0c9fbf8de980c046568ff11aa3
Yawen Zhang <yawenz@allenai.org>
2025-09-27 13:25:29 -0700

    reduce patch size to 4

--- helios/internal/all_evals.py
+++ helios/internal/all_evals.py
@@ -55,8 +55,6 @@ EVAL_TASKS = {
         norm_stats_from_pretrained=True,
         input_modalities=[Modality.SENTINEL2_L2A.name],
         input_layers=["sentinel2"],
-        # Set patch size as 1 to be consistent with AEF experiments
-        patch_size=1,
     ),
     "m_forestnet": DownstreamTaskConfig(
         dataset="m-forestnet",
@@ -90,10 +88,8 @@ EVAL_TASKS = {
         norm_stats_from_pretrained=True,
         input_modalities=[Modality.SENTINEL1.name],
         input_layers=["sentinel1_ascending"],
-        # Set patch size as 1 to be consistent with AEF experiments
-        patch_size=1,
     ),
-    "nandi_landset": DownstreamTaskConfig(
+    "nandi_landsat": DownstreamTaskConfig(
         dataset="nandi",
         embedding_batch_size=128,
         num_workers=0,
@@ -101,8 +97,6 @@ EVAL_TASKS = {
         norm_stats_from_pretrained=True,
         input_modalities=[Modality.LANDSAT.name],
         input_layers=["landsat"],
-        # Set patch size as 1 to be consistent with AEF experiments
-        patch_size=1,
     ),
     "awf_sentinel2": DownstreamTaskConfig(
         dataset="awf",
