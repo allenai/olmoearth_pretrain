dcb0fe73b32a9b03aaeb413251490eae6f5753ab
hgherzog <henryh@allenai.org>
2025-10-03 03:26:54 +0000

    fix bad default

--- helios/internal/all_evals.py
+++ helios/internal/all_evals.py
@@ -159,7 +159,7 @@ EVAL_TASKS = {
     ),
     "pastis_sentinel1_sentinel2": DownstreamTaskConfig(
         dataset="pastis",
-        embedding_batch_size=512,
+        embedding_batch_size=32,
         probe_batch_size=8,
         num_workers=2,
         pooling_type=PoolingType.MEAN,
