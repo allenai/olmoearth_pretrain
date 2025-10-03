45ebb81f91b9b97cb526e000e74a2f8550418a31
Favyen Bastani <fbastani@perennate.com>
2025-10-03 12:25:55 -0400

    fix upsample

--- helios/evals/models/satlas/satlas.py
+++ helios/evals/models/satlas/satlas.py
@@ -182,11 +182,9 @@ class Satlas(nn.Module):
         processed_inputs, modality = self.prepare_input(masked_helios_sample)
         outputs_list: list[torch.Tensor] = []
         for per_t_input in processed_inputs:
-            # we only take the last feature map from satlas for consistency with
-            # the other models. For segmentation tasks, multi scale feature maps
-            # may be preferred. An example of this is in
-            # https://github.com/allenai/rslearn/blob/master/rslearn/models/swin.py
-            output = self.models[modality](per_t_input)[-1]
+            # we use the first feature map from the fpn+upsample which is the upsampled
+            # features.
+            output = self.models[modality](per_t_input)[0]
             print(output.shape)
             # output shape for atto: (bsz, 320, 7, 7)
             # output shape for tiny: (bsz, 768, 6, 6)
