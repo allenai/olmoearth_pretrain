0b3a7a755c06698d515e3bf4c52d99c02af95984
hgherzog <henryh@allenai.org>
2025-10-15 22:14:53 +0000

    remove todo

--- olmoearth_pretrain/internal/constants.py
+++ olmoearth_pretrain/internal/constants.py
@@ -1,8 +1,5 @@
 """Internal constants for the OlmoEarth Pretrain project."""
 
-
-# TODO: Maybe all of these should be possible to oveerride with env vars?
-
 EVAL_WANDB_PROJECT = "olmoearth_pretrain_knn_and_lp_evals"
 WANDB_ENTITY = "eai-ai2"  # nosec
 EVAL_LAUNCH_PATH = "olmoearth_pretrain/internal/all_evals.py"
