837fd6bd01e6b97604d58c8d9cc1e1a22f587878
Gabriel Tseng <gabriel.tseng@mail.mcgill.ca>
2026-01-29 14:58:06 +0000

    more gpus

--- scripts/2026_01_27_maps_ablation/launch_maps_ablation.sh
+++ scripts/2026_01_27_maps_ablation/launch_maps_ablation.sh
@@ -10,6 +10,7 @@ CLUSTER="ai2/ceres-cirrascale"
 CLUSTERS="[ai2/jupiter-cirrascale-2,ai2/ceres-cirrascale]"
 PRIORITY="high"
 WANDB_PROJECT="2026_01_27_maps_ablation"
+NUM_GPUS=8
 
 # Ablation 1: Remove WORLDCOVER
 python scripts/official/base.py launch ablation_no_worldcover $CLUSTER \
@@ -18,6 +19,7 @@ python scripts/official/base.py launch ablation_no_worldcover $CLUSTER \
     --data_loader.masking_config.strategy_config.only_decode_modalities='[srtm,openstreetmap_raster,wri_canopy_height_map,cdl,worldcereal]' \
     --launch.clusters="$CLUSTERS" \
     --launch.priority=$PRIORITY \
+    --launch.num_gpus=$NUM_GPUS \
     --trainer.callbacks.wandb.project=$WANDB_PROJECT
 
 # Ablation 2: Remove SRTM
@@ -27,6 +29,7 @@ python scripts/official/base.py launch ablation_no_srtm $CLUSTER \
     --data_loader.masking_config.strategy_config.only_decode_modalities='[worldcover,openstreetmap_raster,wri_canopy_height_map,cdl,worldcereal]' \
     --launch.clusters="$CLUSTERS" \
     --launch.priority=$PRIORITY \
+    --launch.num_gpus=$NUM_GPUS \
     --trainer.callbacks.wandb.project=$WANDB_PROJECT
 
 # Ablation 3: Remove OPENSTREETMAP_RASTER
@@ -36,6 +39,7 @@ python scripts/official/base.py launch ablation_no_openstreetmap_raster $CLUSTER
     --data_loader.masking_config.strategy_config.only_decode_modalities='[worldcover,srtm,wri_canopy_height_map,cdl,worldcereal]' \
     --launch.clusters="$CLUSTERS" \
     --launch.priority=$PRIORITY \
+    --launch.num_gpus=$NUM_GPUS \
     --trainer.callbacks.wandb.project=$WANDB_PROJECT
 
 # Ablation 4: Remove WRI_CANOPY_HEIGHT_MAP
@@ -45,6 +49,7 @@ python scripts/official/base.py launch ablation_no_wri_canopy_height_map $CLUSTE
     --data_loader.masking_config.strategy_config.only_decode_modalities='[worldcover,srtm,openstreetmap_raster,cdl,worldcereal]' \
     --launch.clusters="$CLUSTERS" \
     --launch.priority=$PRIORITY \
+    --launch.num_gpus=$NUM_GPUS \
     --trainer.callbacks.wandb.project=$WANDB_PROJECT
 
 # Ablation 5: Remove CDL
@@ -54,6 +59,7 @@ python scripts/official/base.py launch ablation_no_cdl $CLUSTER \
     --data_loader.masking_config.strategy_config.only_decode_modalities='[worldcover,srtm,openstreetmap_raster,wri_canopy_height_map,worldcereal]' \
     --launch.clusters="$CLUSTERS" \
     --launch.priority=$PRIORITY \
+    --launch.num_gpus=$NUM_GPUS \
     --trainer.callbacks.wandb.project=$WANDB_PROJECT
 
 # Ablation 6: Remove WORLDCEREAL
@@ -63,4 +69,5 @@ python scripts/official/base.py launch ablation_no_worldcereal $CLUSTER \
     --data_loader.masking_config.strategy_config.only_decode_modalities='[worldcover,srtm,openstreetmap_raster,wri_canopy_height_map,cdl]' \
     --launch.clusters="$CLUSTERS" \
     --launch.priority=$PRIORITY \
+    --launch.num_gpus=$NUM_GPUS \
     --trainer.callbacks.wandb.project=$WANDB_PROJECT
