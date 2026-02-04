ab19b6a61bd9c78fe0bd1bfd14d0332f21d846f9
Piper Wolters <piperwolters@gmail.com>
2026-02-03 17:22:15 -0800

    hm

--- olmoearth_pretrain/train/callbacks/wandb.py
+++ olmoearth_pretrain/train/callbacks/wandb.py
@@ -104,7 +104,7 @@ class OlmoEarthWandBCallback(WandBCallback):
                 config=self.config,
                 id=resume_id,
                 resume="allow",
-                settings=self.wandb.Settings(init_timeout=240),
+                settings=self.wandb.Settings(init_timeout=600),
             )
 
             if not resume_id and self.restart_on_same_run:
