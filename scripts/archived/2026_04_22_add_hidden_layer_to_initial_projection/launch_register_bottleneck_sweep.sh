f752b3d43f981821e9de14eb0207c3ea70f11e54
Gabriel Tseng <gabriel.tseng@mail.mcgill.ca>
2026-05-27 08:43:22 -0700

    d768

--- scripts/archived/2026_04_22_add_hidden_layer_to_initial_projection/launch_register_bottleneck_sweep.sh
+++ scripts/archived/2026_04_22_add_hidden_layer_to_initial_projection/launch_register_bottleneck_sweep.sh
@@ -6,9 +6,9 @@
 #   - register_read_depth=1, register_latent_depth=4, register_num_heads=12 (= encoder)
 #   - low-weight register supervision (supervision weight x0.1, set in the script)
 #
-# Sweep (6 runs): register_grid_size in {16, 32} x register_dim in {192, 288, 528}.
+# Sweep (8 runs): register_grid_size in {16, 32} x register_dim in {192, 288, 528, 768}.
 # dims are multiples of 48 so head_dim = dim/12 is divisible by 4 (required by 2D RoPE);
-# head_dims are 16, 24, 44 respectively.
+# head_dims are 16, 24, 44, 64 respectively (768 = full model width = no width bottleneck).
 set -e
 
 SCRIPT="scripts/archived/2026_04_22_add_hidden_layer_to_initial_projection/hidden1_supervision_register_bottleneck.py"
@@ -18,7 +18,7 @@ WANDB_PROJECT="--trainer.callbacks.wandb.project=2026_04_22_add_hidden_layer_to_
 ROPE="--model.encoder_config.rope_coordinate_scale=0.25 --model.decoder_config.rope_coordinate_scale=0.25"
 
 for GRID in 16 32; do
-    for DIM in 192 288 528; do
+    for DIM in 192 288 528 768; do
         python "$SCRIPT" launch "regbtl_base10k_scale0.25_g${GRID}_d${DIM}" "$CLUSTER" \
             $LAUNCH_ARGS \
             $WANDB_PROJECT \
