6d3fb0399e8a15976ef8101a6a354ef74702472f
Favyen Bastani <favyenb@allenai.org>
2026-07-01 11:04:55 -0700

    fix test

--- tests/integration/eval/test_probe.py
+++ tests/integration/eval/test_probe.py
@@ -105,9 +105,14 @@ def test_probe_seg() -> None:
     assert isinstance(result.test_result, EvalResult)
 
     # Segmentation returns EvalResult with segmentation metrics
-    expected_metrics = {"miou", "overall_acc", "macro_acc", "macro_f1", "micro_f1"} | {
-        f"f1_class_{i}" for i in range(config.num_classes)
-    }
+    expected_metrics = {
+        "miou",
+        "overall_acc",
+        "macro_acc",
+        "macro_f1",
+        "micro_f1",
+        "auroc",
+    } | {f"f1_class_{i}" for i in range(config.num_classes)}
     assert set(result.val_result.metrics.keys()) == expected_metrics
     assert set(result.test_result.metrics.keys()) == expected_metrics
 
